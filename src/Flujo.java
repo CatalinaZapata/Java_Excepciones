44678f6b2b0df15f981bb69d7792f69cd6e3a502
Cata <catalina.zapata@usach.cl>
2022-12-13 11:15:18 -0300

    V0R4: Checked y unchecked

--- src/Flujo.java
+++ src/Flujo.java
@@ -1,21 +1,18 @@
 public class Flujo {
 	public static void main(String[] args) {
-		System.out.println("Inicio main");
 		metodo1();
 		System.out.println("Fin de main");
 	}
 	
-	private static void metodo1() {
+	private static void metodo1() throws MyException{
 		System.out.println("Inicio de metodo1");
 		metodo2();
 		System.out.println("Fin de metodo1");
 	}
 	
-	private static void metodo2() {
+	private static void metodo2() throws MyException{
 		System.out.println("Inicio de metodo2");
-		Cuenta cuenta = new Cuenta();
-		throw new ArithmeticException();
-		
+		throw new MyException("Mi excepcion fue lanzada");
 	}
 
 
